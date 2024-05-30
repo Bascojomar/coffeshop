4012121f807bea0e46fea83cefe967b0e857b7d7
Bascojomar <113278770+Bascojomar@users.noreply.github.com>
2024-05-30 10:32:58 +0800

    as
    sa

--- backend/logout.php
+++ backend/logout.php
@@ -1,6 +1,5 @@
 <?php 
 	include 'database.php';
-	$update = mysqli_query($conn, "UPDATE tbl_users WHERE position = 'admin'");
 	session_start();
 	session_destroy();
 	header("location:../index.php");
--- backend/user_logout.php
+++ backend/user_logout.php
@@ -1,7 +1,6 @@
 <?php 
 include 'database.php';
 include "session.php";
-$update = mysqli_query($conn, "UPDATE tbl_users WHERE username = '$_SESSION[username]'");
 session_destroy();
 header("location:../../index.php");
  ?>
